9d426ce92dda20fa44ca699f9a61db73748ddb2f
Ntye <ntye.nina@gmail.com>
2025-11-18 13:44:09 +0100

    latest update nina

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,18 @@ import {DEFAULT_ORGANIZATION} from "./config/firebase.ts";
 import './utils/i18n';
 import {SearchProvider} from "./context/SearchContext.tsx";
 import {AuthProvider} from "./context/AuthContext.tsx";
+import { I18nProvider } from './context/I18nContext.tsx';
 
 function App() {
   return (
     <AuthProvider>
-      <ConfigProvider orgName={DEFAULT_ORGANIZATION}>
-        <SearchProvider>
-          <RouterProvider router={routes} />
-        </SearchProvider>
-      </ConfigProvider>
+      <I18nProvider>
+        <ConfigProvider orgName={DEFAULT_ORGANIZATION}>
+          <SearchProvider>
+            <RouterProvider router={routes} />
+          </SearchProvider>
+        </ConfigProvider>
+      </I18nProvider>
     </AuthProvider>
 
   );
